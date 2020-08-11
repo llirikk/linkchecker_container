0a33d47f0fa8c8a9ce9adce7d6f8e6fb39d970fb
llirikk <krasaviniko@gmail.com>
2020-08-11 17:34:01 +0300

    (fix) plugins permission denied

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --user $(id -u):$(id -g) -v $(pwd)/out:/out -v $(pwd)/in:/in -v $(pwd)/conf:/conf linkchecker:latest $1
+docker run --user $(id -u):$(id -g) -v $(pwd)/out:/out -v $(pwd)/in:/in -v $(pwd)/conf:/conf -v $(pwd)/plugins:/.local/share/linkchecker/plugins linkchecker:latest $1
